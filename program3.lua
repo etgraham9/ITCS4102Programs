fedb5cb37cced5a0fb9d8679d128d4f1bbca05c6
Jiale-Wu <Jwu45@Charlotte.edu>
2025-10-08 18:50:36 -0400

    Exception handling exception using a sum function

--- program3.lua
+++ program3.lua
@@ -1,1 +1,24 @@
 -- This program wil showcase exception handling or concurrency.
+
+function Sum(a, b)
+    if type(a) == "number" and type(b) == "number" then
+        return a + b
+    else
+        error("Your first or second input is not a number")
+    end
+end
+
+print("Type first number:")
+local first_num = tonumber(io.read())  -- convert string to number
+print("Type second number:")
+local second_num = tonumber(io.read())
+
+local success, result = pcall(function()
+    return Sum(first_num, second_num)
+end)
+
+if success then
+    print("Result: " .. result)
+else
+    print("Error: " .. result)
+end
